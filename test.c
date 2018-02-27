98d1300de0f59351a052f43bb619a1b272534daa
Andrius Kelly <kellandr@os1.engr.oregonstate.edu>
2018-02-26 17:31:20 -0800

    fix empty input bug

--- test.c
+++ test.c
@@ -3,7 +3,6 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <sys/types.h>
@@ -15,49 +14,61 @@
 #define BUF_LEN 2048
 #define ARG_TOT 512
 
+//Signal handlers
+
+
 //PROTOTYPES
 void parseUserInput( char* userInput, char* commandArgs[], char* commandOpts[], int* numArgs, int* numOpts) ;
 
 
 int main (void) {
 
-	int i;
+	//VARIABLE DECLARATIONS
+		int i;
 
-    char* userInput = NULL;
-    size_t maxUserInput = BUF_LEN;
-	int inputLength;
+		char* userInput = NULL;
+		size_t maxUserInput = BUF_LEN;
+		int inputLength;
 
-	pid_t spawnPid;
-    int childExitMethod;
-	int exitStatus;
+		pid_t spawnId;
+		int childExitMethod;
+		int exitStatus;
 
-	//store command arguments and options as array elements
-    char* commandArgs[ARG_TOT];
-	int numArgs;
-    char* commandOpts[ARG_TOT];
-	int numOpts;
+		//store command arguments and options as array elements
+		char* commandArgs[ARG_TOT];
+		int numArgs;
+		char* commandOpts[ARG_TOT];
+		int numOpts;
 
-	//flag to restart main loop
-	int badCommand;
+		//flag to restart main loop
+		int badCommand;
+		
+		//pointer to input/output filename
+		char* filename;
+		int file_d;
+
+		//working directory string
+		char cwd[BUF_LEN];
+		int pauseShell;
+		
+		//save default file descriptors
+		int default_0 = dup(0);
+		int default_1 = dup(1);
+		int default_2 = dup(2);
+
+		//signal catchers
+		struct sigaction SIGINT_action = {0};
+		struct sigaction SIGSTP_action = {0};
+
+	//TODO: kill child zombie when child terminates
+
+	//setup signal actions
+	SIGINT_action.sa_handler = SIG_IGN;
+	//sigfillset(&SIGINT_action.sa_mask);
+	//SIGINT_action.sa_flags = 0;
 	
-	//pointer to input/output filename
-	char* filename;
-	int file_d;
-
-	//working directory string
-	char cwd[BUF_LEN];
-	int pauseShell;
-	
-	//save default file descriptors
-	int default_0 = dup(0);
-	int default_1 = dup(1);
-	int default_2 = dup(2);
-
-	//signal catchers
-	struct sigaction SIGINT_action = {0};
-	struct sigaction SIGSTP_action = {0};
-
-	//TODO: kill child when terminated
+	//bind signal actions to signals
+	sigaction(SIGINT, &SIGINT_action, NULL);
 
 	//save this process id as int and string
 	int thisPid = getpid();
@@ -90,114 +101,125 @@ int main (void) {
 		inputLength = getline( &userInput, &maxUserInput, stdin);
         userInput[strlen(userInput) - 1]  = '\0';
         
-		parseUserInput(userInput, commandArgs, commandOpts, &numArgs, &numOpts);
+		if ( strcmp(userInput, "") != 0 ) {
 
-		//replace $$ with pid
-		for( i=0; i < numArgs; i++) {
-			if( strcmp(commandArgs[i], "$$") == 0 ) {
-				commandArgs[i] = pidString;	
-			}
-		}
-		
-		//handle exit, cd, status, comments 
-		if( strcmp(commandArgs[0], "exit") == 0 ) {
-            break;
-        }
-		if( strcmp(commandArgs[0], "cd") == 0 ) {
-			if ( chdir( commandArgs[1] ) != 0 ) {
-				fprintf( stderr, "could not cd to %s\n", commandArgs[1]);
-        		fflush(stderr);
-			}
-		}
-		if( strcmp(commandArgs[0], "status") == 0 ) {
-			//TODO: print out either the exit status or the terminating signal of the last foreground process (not both, processes killed by signals do not have exit statuses!) ran by your shell.
-		}
-		if ( commandArgs[0][0] == '#' ) {
-			continue;
-		}
+			parseUserInput(userInput, commandArgs, commandOpts, &numArgs, &numOpts);
 
-		
-		//check options for file input/output
-		for ( i=0; i<numOpts; i++ ) {
+			//replace $$ with pid
+			for( i=0; i < numArgs; i++) {
+				if( strcmp(commandArgs[i], "$$") == 0 ) {
+					commandArgs[i] = pidString;	
+				}
+			}
 			
-			filename = commandOpts[i+1];
+			//handle exit, cd, status, comments 
+			if( strcmp(commandArgs[0], "exit") == 0 ) {
+				break;
+			}
+			if( strcmp(commandArgs[0], "cd") == 0 ) {
+				if ( chdir( commandArgs[1] ) != 0 ) {
+					fprintf( stderr, "could not cd to %s\n", commandArgs[1]);
+					fflush(stderr);
+				}
+			}
+			if( strcmp(commandArgs[0], "status") == 0 ) {
+				//TODO: print out either the exit status or the terminating signal of the last foreground process (not both, processes killed by signals do not have exit statuses!) ran by your shell.
+			}
+			if ( commandArgs[0][0] == '#' ) {
+				continue;
+			}
+
 			
-			//set fd 0 to filename descriptor for reading
-			if( strcmp( commandOpts[i], "<") == 0 ) {
-			
-				file_d = open(filename, O_RDONLY);
+			//check options for file input/output
+			for ( i=0; i<numOpts; i++ ) {
 				
-				if ( file_d < 0 ) {
-					fprintf(stderr, "error opening %s for read", filename);
-					fflush(stderr);
-					badCommand = true;
-				} 
-				else {
-					dup2(file_d, 0);
-				}
-			}
-			//set fd 1 to filename descriptor for writing
-			else if ( strcmp( commandOpts[i], ">") == 0 ) {
-			
-				file_d = open( filename, O_WRONLY | O_CREAT | O_TRUNC, 0755 );
-			
-				if ( file_d < 0 ) {
-					fprintf(stderr, "error opening %s for write", filename);
-					fflush(stderr);
-					badCommand = true;
-				} 
-				else {
-					dup2(file_d, 1);
-				}
-			}
-		}
-
-		if(badCommand) continue;		
-		
-		//get cwd
-		if ( getcwd(cwd, sizeof(cwd) ) == NULL ) {
-			perror("could not get current working directory");
-			exit(1);
-		}
-
-		//if last option is &, make child process background (don't pause Shell)
-		if( numOpts > 0 && strcmp( commandOpts[numOpts - 1], "&" ) == 0 ){
-			pauseShell = false;
-		}
-
-        //when command received, fork current process and exec commend in child
-		childExitMethod = -5;
-		spawnPid = fork();
-        switch (spawnPid) {
-			//error
-            case -1:                    
-                perror("process fork error!");
-				exit(1); break;
-			//child
-			case 0:
-				execvp( commandArgs[0], commandArgs );
-				perror("CHILD: exec failure!\n");
-				exit(2); break;
-			//parent
-			default:
-                //halt program if not background
-				if (pauseShell) {
-	                waitpid(spawnPid, &childExitMethod, 0);
-    			}
-				if( WIFEXITED(childExitMethod) ) {
-					exitStatus = WEXITSTATUS( childExitMethod );
-					if (exitStatus != 0) {
-						fprintf(stderr, "child process exited with %d\n", exitStatus); 
+				filename = commandOpts[i+1];
+				
+				//set fd 0 to filename descriptor for reading
+				if( strcmp( commandOpts[i], "<") == 0 ) {
+				
+					file_d = open(filename, O_RDONLY);
+					
+					if ( file_d < 0 ) {
+						fprintf(stderr, "error opening %s for read", filename);
 						fflush(stderr);
+						badCommand = true;
+					} 
+					else {
+						dup2(file_d, 0);
 					}
 				}
-				else {
-					perror("child exited by signal\n");
-					fflush(stderr);
+				//set fd 1 to filename descriptor for writing
+				else if ( strcmp( commandOpts[i], ">") == 0 ) {
+				
+					file_d = open( filename, O_WRONLY | O_CREAT | O_TRUNC, 0755 );
+				
+					if ( file_d < 0 ) {
+						fprintf(stderr, "error opening %s for write", filename);
+						fflush(stderr);
+						badCommand = true;
+					} 
+					else {
+						dup2(file_d, 1);
+					}
 				}
-                break;
-        }
+			}
 
+			if(badCommand) continue;		
+			
+			//get cwd
+			if ( getcwd(cwd, sizeof(cwd) ) == NULL ) {
+				perror("could not get current working directory");
+				exit(1);
+			}
+
+			//if last option is &, make child process background (don't pause Shell)
+			if( numOpts > 0 && strcmp( commandOpts[numOpts - 1], "&" ) == 0 ){
+				pauseShell = false;
+				//TODO: redirect bg i/o to dev/null
+				
+			}
+
+			//when command received, fork current process and exec commend in child
+			childExitMethod = -5;
+			spawnId = fork();
+		
+			switch (spawnId) {
+				//error
+				case -1:                    
+					perror("process fork error!");
+					exit(1); break;
+				//child
+				case 0:
+					execvp( commandArgs[0], commandArgs );
+					perror("CHILD: exec failure!\n");
+					exit(2); break;
+				//parent
+				default:
+					//halt program if not background
+					if (pauseShell) {
+					   
+					   waitpid(spawnId, &childExitMethod, 0);
+						
+						if( WIFEXITED(childExitMethod) ) {
+							exitStatus = WEXITSTATUS( childExitMethod );
+							if (exitStatus != 0) {
+								fprintf(stderr, "child process exited with %d\n", exitStatus); 
+								fflush(stderr);
+							}
+						}
+						else if( WIFSIGNALED(childExitMethod) ) {
+							perror("child exited by signal\n");
+							fflush(stderr);
+						}
+					}
+					else {
+						printf("%d\n", spawnId);
+					}
+
+					break;
+			}
+		}
     }
 
     free (userInput);
